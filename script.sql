e9204462f3eb0a6331dd87a7577d0b0b70aa4474
Syders <stevenalpha31@gmail.com>
2018-01-12 09:46:47 +0100

    Script Complet

--- script.sql
+++ script.sql
@@ -23,9 +23,11 @@ CREATE TABLE FilDiscussion
 CREATE TABLE Message
   (IdMessage INT(4),
    Texte VARCHAR(200),
+   Lue INT(1),
    IdUtilisateur VARCHAR(30),
    IdFilDiscussion INT(3),
    CONSTRAINT pk_message PRIMARY KEY(IdMessage),
+   CONSTRAINT ck_lue CHECK (Lue<=2 AND Lue>=1),
    CONSTRAINT fk_message_utilisateur FOREIGN KEY(IdUtilisateur)
                          REFERENCES Utilisateur(IdUtilisateur),
    CONSTRAINT fk_message_fildiscussion FOREIGN KEY(IdFilDiscussion)
@@ -40,3 +42,49 @@ CREATE TABLE Appartient
    CONSTRAINT fk_appartient_groupeutilisateur FOREIGN KEY(NomGroupe)
                              REFERENCES GroupeUtilisateur(NomGroupe)   
 );
+
+INSERT INTO Utilisateur (IdUtilisateur, prenom, nom, motDePasse)
+VALUES
+('Alpha','Aaron','James','A158'),
+('Bravo','Jones','George','&dsq'),
+('Charlie','Vioe','Patrick','m84?'),
+('Delta','Parrot','Sael',':)hj'),
+('Echo','Farraday','Ilo','kji7'),
+('Foxtrot','Geordi','Atori','ndm7'),
+('Golf','Lia','Manon','v9d4'),
+('Hotel','Poal','Julia','hjup'),
+('India','Krap','Masri','hjhk'),
+('Juliett','Asop','Rael','qdfq');
+
+INSERT INTO GroupeUtilisateur (NomGroupe)
+VALUES
+('A'),('Technical'),('B'),('C');
+
+INSERT INTO FilDiscussion (IdFilDiscussion,Titre,IdUtilisateur,NomGroupe)
+VALUES
+(1,'Souci','Alpha','Technical'),
+(2,'Resolu','India','A'),
+(3,'Probleme','Echo','C');
+
+INSERT INTO Appartient (IdUtilisateur, NomGroupe)
+VALUES
+('Alpha','A'),
+('Bravo','B'),
+('Charlie','C'),
+('Delta','Technical'),
+('Echo','A'),
+('Echo','B'),
+('Foxtrot','C'),
+('Golf','B'),
+('Hotel','A'),
+('Hotel','C'),
+('India','Technical'),
+('Juliett','Technical');
+
+INSERT INTO Message (IdMessage,Texte,Lue,IdUtilisateur,IdFilDiscussion)
+VALUES
+(1,'Bonjour problème A3',1,'Alpha',1),
+(2,'J ai pris en compte votre message',1,'India',1),
+(3,'Problème résolu sur A3',1,'India',2),
+(4,'Explosion en cours sur B2',2,'Echo',3),
+(5,'Merci',2,'Alpha',2);
