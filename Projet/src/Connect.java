3f9172d02d23e28fc69ee33c436b724dd0d9fdba
Syders <stevenalpha31@gmail.com>
2018-01-12 09:48:30 +0100

    Add files via upload

--- Projet/src/Connect.java
+++ Projet/src/Connect.java
@@ -6,6 +6,7 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 /**
@@ -28,7 +29,7 @@ public class Connect {
 
       String user = "root";
 
-      String passwd = "";
+      String passwd = "A120120120a";
 
          
 
@@ -57,34 +58,163 @@ public class Connect {
   }
   
   public void ajouterUtilisateur(String IdUtilisateur, String Nom, String Prenom, String MotDePasse) {
-      inserer("INSERT INTO Utilisateur VALUES(" + IdUtilisateur + ", '" + Nom + "', '" + Prenom + ", '" + MotDePasse + "')");
+      inserer("INSERT INTO Utilisateur VALUES('" + IdUtilisateur + "', '" + Nom + "', '" + Prenom + ", '" + MotDePasse + "')");
   }
 
   public void ajouterGroupeUtilisateur(String NomGroupe) {
-      inserer("INSERT INTO GroupeUtilisateur VALUES(" + NomGroupe + "')");
+      inserer("INSERT INTO GroupeUtilisateur VALUES('" + NomGroupe + "')");
   }
   
   public void ajouterFilDiscussion(int IdFilDiscussion, String Titre, String IdUtilisateur, String NomGroupe) {
-      inserer("INSERT INTO FilDiscussion VALUES(" + IdFilDiscussion + ", '" + Titre + "', " + IdUtilisateur + ", " + NomGroupe + ")");
+      inserer("INSERT INTO FilDiscussion VALUES(" + IdFilDiscussion + ", '" + Titre + "', '" + IdUtilisateur + "', '" + NomGroupe + "')");
   }
 
   public void ajouterMessage(int IdMessage, String Texte, String IdUtilisateur, int IdFilDiscussion) {
-      inserer("INSERT INTO Message VALUES(" + IdMessage + ", '" + Texte + "', " + IdUtilisateur + ", " + IdFilDiscussion + ")");
+      inserer("INSERT INTO Message VALUES(" + IdMessage + ", '" + Texte + "', '" + IdUtilisateur + "', " + IdFilDiscussion + ")");
   }
   
   public void ajouterAppartient(String IdUtilisateur, String NomGroupe) {
-      inserer("INSERT INTO Appartient VALUES(" + IdUtilisateur + ", " + NomGroupe + ")");
+      inserer("INSERT INTO Appartient VALUES('" + IdUtilisateur + "', '" + NomGroupe + "')");
   }
 
   public void supprimerUtilisateur(String IdUtilisateur) {
-      inserer("DELETE FROM Utilisateur WHERE IdUtilisateur = " + IdUtilisateur);
+      inserer("DELETE FROM Utilisateur WHERE IdUtilisateur = '" + IdUtilisateur + "'");
   }
 
   public void supprimerGroupeUtilisateur(String NomGroupe) {
-      inserer("DELETE FROM GroupeUtilisateur WHERE NomGroupe = " + NomGroupe);
+      inserer("DELETE FROM GroupeUtilisateur WHERE NomGroupe = '" + NomGroupe + "'");
   }
 
-  public void supprimerAppartient(int IdUtilisateur, String NomGroupe) {
-      inserer("DELETE FROM Appartient WHERE IdUtilisateur = " + IdUtilisateur + " and NomGroupe= " + NomGroupe);
+  public void supprimerAppartient(String IdUtilisateur, String NomGroupe) {
+      inserer("DELETE FROM Appartient WHERE IdUtilisateur = '" + IdUtilisateur + "' and IdGrUtilisateur = '" + NomGroupe + "'");
+  }
+  
+    private int selectInt(String chaine, String champ) {
+        int val = -1;
+    try {
+
+
+         
+
+      String url = "jdbc:mysql://localhost:3306/bddProjet";
+
+      String user = "root";
+
+      String passwd = "A120120120a";
+
+         
+
+      Connection conn = DriverManager.getConnection(url, user, passwd);
+
+         
+
+      //Création d'un objet Statement
+
+      Statement state = conn.createStatement();
+
+      //L'objet ResultSet contient le résultat de la requête SQL
+
+      ResultSet result = state.executeQuery(chaine);
+      
+      while(result.next()){         
+          val = result.getInt(champ);
+      }
+
+
+      result.close();
+
+      state.close();
+
+         
+
+    } catch (Exception e) {
+
+      e.printStackTrace();
+
+    }    
+
+    return val;
+  }
+    
+  private String selectString(String chaine, String champ) {
+      String val = "";
+    try {
+
+
+         
+
+      String url = "jdbc:mysql://localhost:3306/bddProjet";
+
+      String user = "root";
+
+      String passwd = "A120120120a";
+
+         
+
+      Connection conn = DriverManager.getConnection(url, user, passwd);
+
+         
+
+      //Création d'un objet Statement
+
+      Statement state = conn.createStatement();
+
+      //L'objet ResultSet contient le résultat de la requête SQL
+
+      ResultSet result = state.executeQuery(chaine);
+
+      while(result.next()){         
+          val = result.getString(champ);
+      }
+
+
+      result.close();
+
+      state.close();
+
+         
+
+    } catch (Exception e) {
+
+      e.printStackTrace();
+
+    }      
+    return val;
+  }
+  
+  public String utilisateurNom(String IdUtilisateur) {
+      return selectString("SELECT Nom FROM Utilisateur WHERE IdUtilisateur = '" + IdUtilisateur + "'", "Nom");
+  }
+  
+  public String utilisateurPrenom(String IdUtilisateur) {
+      return selectString("SELECT Prenom FROM Utilisateur WHERE IdUtilisateur = '" + IdUtilisateur + "'", "Prenom");
+  }
+  
+  public String utilisateurMotDePasse(String IdUtilisateur) {
+      return selectString("SELECT MotDePasse FROM Utilisateur WHERE IdUtilisateur = '" + IdUtilisateur + "'", "MotDePasse");
+  }
+  
+  public String filDiscussionTitre(int IdFilDiscussion) {
+      return selectString("SELECT Titre FROM FilDiscussion WHERE IdFilDiscussion = " + IdFilDiscussion, "Titre");
+  }
+  
+  public String filDiscussionIdUtilisateur(int IdFilDiscussion) {
+      return selectString("SELECT IdUtilisateur FROM FilDiscussion WHERE IdFilDiscussion = " + IdFilDiscussion, "IdUtilisateur");
+  }
+  
+  public String filDiscussionNomGroupe(int IdFilDiscussion) {
+      return selectString("SELECT NomGroupe FROM FilDiscussion WHERE IdFilDiscussion = " + IdFilDiscussion, "NomGroupe");
+  }
+  
+  public String messageTexte(int IdMessage) {
+      return selectString("SELECT Texte FROM Message WHERE IdMessage = " + IdMessage, "Texte");
+  }
+  
+  public String messageIdUtilisateur(int IdMessage) {
+      return selectString("SELECT IdUtilisateur FROM Message WHERE IdMessage = " + IdMessage, "IdUtilisateur");
+  }
+  
+  public int messageIdFilDiscussion(int IdMessage) {
+      return selectInt("SELECT IdFilDiscussion FROM Message WHERE IdMessage = " + IdMessage, "IdFilDiscussion");
   }
 }
