6835f7baff75f53f6de918dfc26563b4642f4968
Ales7 <ales75016@gmail.com>
2018-01-12 01:05:19 +0100

    Update script.sql

--- script.sql
+++ script.sql
@@ -23,7 +23,7 @@ CREATE TABLE FilDiscussion
 CREATE TABLE Message
   (IdMessage INT(4),
    Texte VARCHAR(200),
-   DatePublication DATE,
+   DatePublication DATETIME,
    IdUtilisateur VARCHAR(30),
    IdFilDiscussion INT(3),
    CONSTRAINT pk_message PRIMARY KEY(IdMessage),
